1b7a7cc4e79b7b7d9df4843a750137af843f1949
nkubana0 <i.shema@alustudent.com>
2024-05-12 16:11:46 +0200

    categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import ShopCategory from "./Pages/shopCategory";
 import Product from "./Pages/product";
 import Cart from "./Pages/cart";
 import Login from "./Pages/login";
+import Footer from "./Components/footer/footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kids_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,16 +19,25 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category = "men" />} />
-          <Route path="/womens" element={<ShopCategory category = "women" />} />
-          <Route path="/kids" element={<ShopCategory category = "kid" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
           <Route path="/product" element={<Product />} />
           <Route path=":productId" element={<Product />} />
           <Route />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/login" element={<Login/>} />
-
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
