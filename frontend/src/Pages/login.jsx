789f01f0795d16511884685312f79e486cc2145a
nkubana0 <i.shema@alustudent.com>
2024-05-12 16:11:46 +0200

    categories

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './css/login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <div className="login-container">
+        <h1>Sign Up</h1>
+        <div className="login-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+          <button>Continue</button>
+          <p className='login-login'>Already Have An Acoount? <span>Login Here!</span></p>
+          <div className="agree">
+            <input type="checkbox" name='' id='' />
+            <p>By continuing I agree to use the terms of Use and Privacy Policy</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/shop.jsx
+++ frontend/src/Pages/shop.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from '../Components/Hero/hero'
+import Popular from '../Components/popular/popular'
+import Offers from '../Components/Offers/offers'
+import NewCollections from '../Components/NewCollections/newCollections'
+import NewsLetter from '../Components/newLetter/newsLetter'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
+    </div>
   )
 }
 
