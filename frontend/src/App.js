518d377934094896a9205bccd487685de05634ce
nkubana0 <i.shema@alustudent.com>
2024-05-17 09:47:18 +0200

    cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,7 +32,7 @@ function App() {
             element={<ShopCategory banner={kids_banner} category="kid" />}
           />
           <Route path="/product" element={<Product />} />
-          <Route path=":productId" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
---
+++ frontend/src/Components/DescriptionBox/description.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./description.css";
+
+const Description = () => {
+  return (
+    <div className="description">
+      <div className="navigator">
+        <div className="nav-box">Description</div>
+        <div className="nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="description">
+        <p>
+          Lore Epsom dolor sit amet, consectetur adipiscing elit. Fusce
+          hendrerit, nulla ut sollicitudin porta, odio leo vehicula lectus, sit
+          amet condimentum ipsum libero id magna. Sed gravida est sit amet ante
+          eleifend, sed fermentum felis molestie. Pellentesque habitant morbi
+          tristique senectus et netus et malesuada fames ac turpis egestas.
+        </p>
+        <p>
+          Lore Epsom dolor sit amet, consectetur adipiscing elit. Fusce
+          hendrerit, nulla ut sollicitudin porta, odio leo vehicula lectus, sit
+          amet condimentum ipsum libero id magna. Sed gravida est sit amet ante
+          eleifend, sed fermentum felis molestie. Pellentesque habitant morbi
+          tristique senectus et netus et malesuada fames ac turpis egestas.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
