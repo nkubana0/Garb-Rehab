120a218d8cf7ff052a18f5e16915d58b1c2f8460
nkubana0 <i.shema@alustudent.com>
2024-06-04 19:54:29 +0200

    responsive:3:38

--- frontend/src/Components/item/item.jsx
+++ frontend/src/Components/item/item.jsx
@@ -6,7 +6,7 @@ const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        <img onClick={window.scrollTo(0,0)} src={props.image} alt="" />
+        <img onClick={window.scrollTo(0,0)} src={props.image} alt="" className="item-image" />
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
