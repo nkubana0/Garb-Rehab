6737d06c5dd4d9e72258d148ea39368dd4a05af8
nkubana0 <i.shema@alustudent.com>
2024-06-23 20:32:14 +0200

    7:56:56

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import './css/login.css'
+import React, { useState } from "react";
+import "./css/login.css";
 
 const Login = () => {
+  const [state, setState] = useState("Login");
+  const login = async ()=> {
+    console.log("Login Function Executed");
+  }
+
+  const signup = async () => {
+    console.log("Signup Function Executed");
+  }
+
   return (
-    <div className='login'>
+    <div className="login">
       <div className="login-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="login-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
-          <button>Continue</button>
-          <p className='login-login'>Already Have An Account? <span>Login Here!</span></p>
+          {state === "Sign Up" ? (
+            <input type="text" placeholder="Your Name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <button onClick={()=>{state === "Login"?login():signup()}}>Continue</button>
+          {state === "Sign Up" ? (
+            <p className="login-login">
+              Already Have An Account? <span onClick={()=>{setState("Login")}}>Login Here!</span>
+            </p>
+          ) : (
+            <p className="login-login">
+              Create An Account! <span onClick={()=>{setState("Sign Up")}}>Click Here!</span>
+            </p>
+          )}
           <div className="agree">
-            <input type="checkbox" name='' id='' className='checkbox' />
-            <p>By continuing I agree to use the terms of Use and Privacy Policy</p>
+            <input type="checkbox" name="" id="" className="checkbox" />
+            <p>
+              By continuing I agree to use the terms of Use and Privacy Policy
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
