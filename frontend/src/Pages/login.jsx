bf5b24973480f0dd4dc1830cb3f30e387d1d1fdd
nkubana0 <i.shema@alustudent.com>
2024-06-24 21:04:43 +0200

    8:08:45

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -3,13 +3,42 @@ import "./css/login.css";
 
 const Login = () => {
   const [state, setState] = useState("Login");
-  const login = async ()=> {
-    console.log("Login Function Executed");
-  }
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const login = async () => {
+    console.log("Login Function Executed", formData);
+  };
 
   const signup = async () => {
-    console.log("Signup Function Executed");
-  }
+    console.log("Signup Function Executed", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+      if(responseData.success){
+        localStorage.setItem('auth-token',responseData.token);
+        window.location.replace("/");
+      }
+      else{
+        alert(responseData.errors)
+      }
+  };
 
   return (
     <div className="login">
@@ -17,20 +46,58 @@ const Login = () => {
         <h1>{state}</h1>
         <div className="login-fields">
           {state === "Sign Up" ? (
-            <input type="text" placeholder="Your Name" />
+            <input
+              name="username"
+              value={formData.username}
+              onChange={changeHandler}
+              type="text"
+              placeholder="Your Name"
+            />
           ) : (
             <></>
           )}
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
-          <button onClick={()=>{state === "Login"?login():signup()}}>Continue</button>
+          <input
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+            type="password"
+            placeholder="Password"
+          />
+          <button
+            onClick={() => {
+              state === "Login" ? login() : signup();
+            }}
+          >
+            Continue
+          </button>
           {state === "Sign Up" ? (
             <p className="login-login">
-              Already Have An Account? <span onClick={()=>{setState("Login")}}>Login Here!</span>
+              Already Have An Account?{" "}
+              <span
+                onClick={() => {
+                  setState("Login");
+                }}
+              >
+                Login Here!
+              </span>
             </p>
           ) : (
             <p className="login-login">
-              Create An Account! <span onClick={()=>{setState("Sign Up")}}>Click Here!</span>
+              Create An Account!{" "}
+              <span
+                onClick={() => {
+                  setState("Sign Up");
+                }}
+              >
+                Click Here!
+              </span>
             </p>
           )}
           <div className="agree">
