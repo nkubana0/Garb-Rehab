f2749a88338e08863df28a4f4494aa13d097312a
nkubana0 <i.shema@alustudent.com>
2024-07-01 13:40:21 +0200

    image urls fixed

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const login = async () => {
     console.log("Login Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login", {
+    await fetch("https://garb-rehab-backend.onrender.com/login", {
       method: "POST",
       headers: {
         Accept: "application/form-data",
