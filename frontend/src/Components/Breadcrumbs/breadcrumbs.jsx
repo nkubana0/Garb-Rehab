8b4c1b4a19df0ed5c5450ba325ef093a43bbb170
nkubana0 <i.shema@alustudent.com>
2024-07-21 14:23:52 +0200

    url change

--- frontend/src/Components/Breadcrumbs/breadcrumbs.jsx
+++ frontend/src/Components/Breadcrumbs/breadcrumbs.jsx
@@ -4,10 +4,15 @@ import arrow_icon from "../Assets/breadcrum_arrow.png";
 
 const Breadcrumb = (props) => {
   const { product } = props;
+
+  if (!product || !product.category || !product.name) {
+    return <div className="breadcrumbs">Loading...</div>; // or any fallback UI
+  }
+
   return (
     <div className="breadcrumbs">
-      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />{" "}
-      {product.category} <img src={arrow_icon} alt="" /> {product.name}
+      HOME <img src={arrow_icon} alt="arrow icon" /> SHOP <img src={arrow_icon} alt="arrow icon" />{" "}
+      {product.category} <img src={arrow_icon} alt="arrow icon" /> {product.name}
     </div>
   );
 };
