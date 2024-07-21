ce0ba6b207eae5d21e533d195e690978bfda9020
nkubana0 <i.shema@alustudent.com>
2024-07-21 14:20:07 +0200

    url change

--- frontend/src/Pages/shopCategory.jsx
+++ frontend/src/Pages/shopCategory.jsx
@@ -1,22 +1,13 @@
 import React, { useContext } from "react";
 import "./css/shopCategory.css";
 import { ShopContext } from "../Context/context";
-import dropdown_icon from "../Components/Assets/dropdown_icon.svg";
+//import dropdown_icon from "../Components/Assets/dropdown_icon.svg";
 import Item from "../Components/item/item";
 
 const ShopCategory = (props) => {
   const { all_products } = useContext(ShopContext);
   return (
     <div className="shop-category">
-      <div className="shopcategory-indexSort">
-        <p>
-          <span>Showing 1-12</span> out of 36 products
-        </p>
-        <div className="shopcategory-sort">
-          <p>Sort by </p>
-          <img src={dropdown_icon} alt="" className="dropdown" />
-        </div>
-      </div>
       <div className="shopcategory-products">
         {all_products.map((item, i) => {
           if (props.category === item.category) {
