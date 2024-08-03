b81c23b8bf670cb565e2421d924f6cbe115a26bb
nkubana0 <i.shema@alustudent.com>
2024-08-03 19:07:51 +0200

    footer

--- frontend/src/Components/footer/footer.jsx
+++ frontend/src/Components/footer/footer.jsx
@@ -1,21 +1,80 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 import './footer.css'
 import footer_logo from "../Assets/logo.png";
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintrest from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import "../Navbar/navbar.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+        const [menu, setMenu] = useState("shop");
+        const menuRef = useRef();
+      
   return (
     <div className='footer'>
         <div className="footer-logo">
             <img src={footer_logo} alt="" />
         </div>
         <ul className="footer-links">
-            <li>Blog</li>
-            <li>Partners</li>
-            <li>About</li>
-            <li>Contact</li>
+        <div className="navbar">
+        <div className="nav-logo">
+        </div>
+        <ul ref={menuRef} className="footer-menu">
+          <li
+            onClick={() => {
+              setMenu("shop");
+            }}
+          >
+            <Link
+              style={{ textDecoration: "none", color: "rgb(216, 215, 210)" }}
+              to="/"
+            >
+              Home
+            </Link>{" "}
+            {menu === "shop" ? <hr /> : <></>}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("mens");
+            }}
+          >
+            <Link
+              style={{ textDecoration: "none", color: "rgb(216, 215, 210)" }}
+              to="/mens"
+            >
+              Men
+            </Link>
+            {menu === "mens" ? <hr /> : <></>}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("womens");
+            }}
+          >
+            <Link
+              style={{ textDecoration: "none", color: "rgb(216, 215, 210)" }}
+              to="/womens"
+            >
+              Women
+            </Link>{" "}
+            {menu === "womens" ? <hr /> : <></>}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("kids");
+            }}
+          >
+            <Link
+              style={{ textDecoration: "none", color: "rgb(216, 215, 210)" }}
+              to="/kids"
+            >
+              Kids
+            </Link>{" "}
+            {menu === "kids" ? <hr /> : <></>}
+          </li>
+        </ul>
+      </div>
         </ul>
         <div className="footer-social-icon">
             <div className="footer-icons-container">
